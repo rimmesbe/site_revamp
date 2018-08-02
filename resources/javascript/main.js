eed54022478d54a512c5f0dd65bd12b8791f1813
rimmesbe <ryanimmesberger@gmail.com>
2018-08-01 21:18:49 -0400

    made header sticky on scroll

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -1,4 +1,4 @@
-// ------project toggle animations------
+// -------------project toggle animations---------------
 
 const projects = document.querySelectorAll(".project");
 
@@ -14,3 +14,23 @@ function toggleActive(e) {
 
 projects.forEach(project => project.addEventListener("click", toggleOpen));
 projects.forEach(project => project.addEventListener("transitionend", toggleActive));
+
+
+// -----------------sticky header-------------------
+
+window.onscroll = function() {myFunction()};
+
+// Get the header
+const header = document.querySelector("header");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+}
